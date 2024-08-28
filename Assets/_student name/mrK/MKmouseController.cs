69651782e18088a13cdcce5397593efd59d7e758
TravisKent <67112365+TravisKent@users.noreply.github.com>
2024-08-28 14:15:59 -0600

    updated after recording

--- Assets/_student name/mrK/MKmouseController.cs
+++ Assets/_student name/mrK/MKmouseController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class MKmouseController : MonoBehaviour
 {
-     // Start is called before the first frame update
+
+    public GameObject card;
+    // Start is called before the first frame update
     void Start()
     {
         
@@ -13,7 +15,42 @@ public class MKmouseController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+       if(Input.GetKeyDown(KeyCode.Space))
+       {
+            Debug.Log("SPACE BAR WAS PRESSED");
+            card.transform.position = new Vector3(3f, 3f, 0f);
+
+       }
+       if(Input.GetKeyDown(KeyCode.W))
+       {
+            Debug.Log("SPACE BAR WAS PRESSED");
+            card.transform.position = new Vector3(3f, 4f, 0f);
+
+       }
+       if(Input.GetKeyDown(KeyCode.S))
+       {
+            Debug.Log("SPACE BAR WAS PRESSED");
+            card.transform.position = new Vector3(3f, 2f, 0f);
+
+       }
+
+       if(Input.GetKeyDown(KeyCode.A))
+       {
+            Debug.Log("SPACE BAR WAS PRESSED");
+            card.transform.position = new Vector3(2f, 3f, 0f);
+
+       }
+       if(Input.GetKeyDown(KeyCode.D))
+       {
+            Debug.Log("SPACE BAR WAS PRESSED");
+            card.transform.position = new Vector3(4f, 3f, 0f);
+
+       }
+
+       if(Input.GetKeyUp(KeyCode.Space))
+       {
+        Debug.Log("Space bar was released");
+       }
     }
 
 }
