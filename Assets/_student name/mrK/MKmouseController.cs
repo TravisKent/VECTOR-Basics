06becd1702a52d2af9ee93ce81a1694186c1ebc4
TravisKent <67112365+TravisKent@users.noreply.github.com>
2024-08-29 10:59:19 -0600

    Created mouse code to select the game object

--- Assets/_student name/mrK/MKmouseController.cs
+++ Assets/_student name/mrK/MKmouseController.cs
@@ -6,6 +6,7 @@ public class MKmouseController : MonoBehaviour
 {
 
     public GameObject card;
+    public GameObject objSelected = null;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,34 @@ public class MKmouseController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+          //display a debug racy where mouse is taking effect
+          Ray newRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+          Debug.DrawRay(newRay.origin, newRay.direction*25, Color.green);
+
+
+          //code for getting left mouse button click
+          if(Input.GetMouseButtonDown(0))
+          {
+               Debug.Log("Mouse Button Pressed");
+               CheckHitObject();
+          }
+          //code for releasing the mouse
+          if(Input.GetMouseButtonUp(0))
+          {
+               Debug.Log("Mouse Button is released");
+          }
+
+
+          //code for dragging with the mouse
+          if(Input.GetMouseButton(0))
+          {
+               Debug.Log("Mouse Button is being Held");
+          }
+
+
+
+//comment out old code
+/*
        if(Input.GetKeyDown(KeyCode.Space))
        {
             Debug.Log("SPACE BAR WAS PRESSED");
@@ -51,6 +80,24 @@ public class MKmouseController : MonoBehaviour
        {
         Debug.Log("Space bar was released");
        }
+
+       */
+    }
+
+
+    //new function goes here:
+    void CheckHitObject()
+    {
+     Debug.Log("Entered Check Hit Object function");
+
+     //create a Ray from the mouse position to check and detect if anything was hit when clicked by the mouse
+     RaycastHit2D hit2D = Physics2D.GetRayIntersection(Camera.main.ScreenPointToRay(Input.mousePosition));
+     if(hit2D.collider !=null)
+     {
+          Debug.Log(hit2D.collider.name +"Was clicked");
+
+          objSelected = hit2D.transform.gameObject;
+     }
     }
 
 }
