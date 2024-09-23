77700a7deb2e044c0ce1205271a4f8338b88cc9c
Leluzawa <sabad824@minidokaschools.org>
2024-09-23 10:47:03 -0700

    franz chess fixed

---
+++ Assets/_student name/Franz/Project 2/FLO_Projectile.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FLO_Projectile : MonoBehaviour
+{
+    /*
+   Rigidbody m_Rigidbody;
+   public float m_Thrust = 20f;
+   float rangeMax = 1.5f;
+
+   void Start()
+   {
+    
+    //Fetch the Rigidbody from the GameObject with this script attached
+    m_Rigidbody = GetComponent<Rigidbody>();
+
+    m_Rigidbody.AddForce(new Vector3(Random.Range(-1*rangeMax,rangeMax), Random.Range(-1*rangeMax,rangeMax), Random.Range(-1*rangeMax,rangeMax))  *  m_Thrust  );
+   }
+
+   void Update()
+   {
+    if(Input.GetKeyDown(KeyCode.Space))
+    {
+        Debug.Log("Add Some Force");
+        //Apply a force to this Rigidbody in random direction
+        m_Rigidbody.AddForce(new Vector3(Random.Range(-1*rangeMax), Random.Range(-1*rangeMax,rangeMax), Random.Range(-1*rangeMax,rangeMax))  *  m_Thrust );
+    }
+}
+*/
+
+}
+
+
+
+
+
+
+
+
+
+
+
