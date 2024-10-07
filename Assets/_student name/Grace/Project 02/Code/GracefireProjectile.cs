fa41e2a06049bd29560740ae327199cf186473a9
GraceC759 <Chen489@minidokaschools.org>
2024-10-07 14:18:16 -0600

    Grace's cat game

---
+++ Assets/_student name/Grace/Project 02/Code/GracefireProjectile.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GracefireProjectile : MonoBehaviour
+{
+    public bool joySTickActivated;
+    public KeyCode p1button01, p1button02;
+    public GameObject projectile;
+    public Transform firePointPosition;
+    public Transform firePointRotation;
+    public float shotDelay;
+    float myTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myTimer=myTimer- Time.deltaTime;
+        if(Input.GetKeyDown(KeyCode.Space) && (myTimer<=0))
+        {
+            myTimer=shotDelay;
+            Instantiate(projectile, firePointPosition.transform.position, firePointRotation.transform.rotation);
+        }
+         if(joySTickActivated)
+        {
+            //fire Button
+             if(Input.GetKeyDown(p1button01))
+            {
+                Debug.Log(p1button01+" pressed");
+                //check if obhect is clicked on
+                //reset the shot timer
+                myTimer = shotDelay;
+                Instantiate(projectile, firePointPosition.transform.position, firePointRotation.transform.rotation);
+            }
+        }
+    }
+}
---
+++ Assets/_student name/Grace/Project 02/GCeasterEgg.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GCeasterEgg : MonoBehaviour
+{
+
+    public bool L1;
+    public bool L2;
+    public bool L3;
+    public bool L4;
+    public bool L5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.G))
+       {
+        Debug.Log("G was pressed");
+        L1 = true;
+       } 
+
+       if(Input.GetKeyDown(KeyCode.R) )
+       {
+        Debug.Log("r was pressed");
+        if(L1==true)
+        {
+            L2 = true;
+        }
+        else
+        {
+            L1 = false;
+            L3 = false;
+            L2 = false;
+            L4 = false;           
+            L5 = false;
+            Debug.Log("EE reset");
+        }
+        
+       }
+      
+
+       if(Input.GetKeyDown(KeyCode.A) )
+       {
+        Debug.Log("a was pressed");
+        if(L1==true && L2==true)
+        {
+            L3 = true;
+        }
+        else
+        {
+            L1 = false;
+            L3 = false;
+            L2 = false;
+            L4 = false;           
+            L5 = false;
+            Debug.Log("EE reset");
+        }
+       
+       }
+       if(Input.GetKeyDown(KeyCode.C) )
+       {
+            Debug.Log("c was pressed");
+            if(L1==true && L2==true && L3==true)
+            {
+                L4 = true;
+            }
+            else
+            {
+                L1 = false;
+                L3 = false;
+                L2 = false;
+                L4 = false;           
+                L5 = false;
+                Debug.Log("EE reset");
+            }
+       }
+       if(Input.GetKeyDown(KeyCode.E) )
+       {
+            Debug.Log("e was pressed");
+            if( L1==true && L2==true && L3==true && L4==true )
+            {
+                L5 = true;
+            }
+            else
+            {
+                L1 = false;
+                L3 = false;
+                L2 = false;
+                L4 = false;           
+                L5 = false;
+                Debug.Log("EE reset");
+            }
+
+       }
+
+ 
+      
+      
+    }
+}
